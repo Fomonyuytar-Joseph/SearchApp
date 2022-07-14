b256f67a8adabe8e37d5f150f5e9e9226a65f083
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-07-14 10:33:49 +0100

    init

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,23 @@
-
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class SearchBar extends Component {
+  state = { term: "" };
   render() {
     return (
-      <div className='ui segment'>
-      <form className='ui form'>
-        <div className='field'>
-          <label>Image Search </label>
-        <input type='text'/>
-        </div>
-      </form>
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search </label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
       </div>
-    )
+    );
   }
 }
 
-export default SearchBar
+export default SearchBar;
