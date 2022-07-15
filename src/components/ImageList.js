15fccbd8c82325e90eb25853824eba470c336b75
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-07-15 11:58:44 +0100

    init

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './ImageList.css'
 
 function ImageList({images}) {
    const displayImages =  images.map(({id , description, urls})=>{
@@ -8,7 +9,7 @@ function ImageList({images}) {
     })
     
   return (
-    <div>{displayImages}</div>
+    <div className='image-list'>{displayImages}</div>
   )
 }
 
